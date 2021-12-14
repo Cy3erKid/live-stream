2667deb3f1bb6878b980c92e19abd37829199e2f
Apiwat Nongwa <contact.apiwat@gmail.com>
2021-12-14 22:36:16 +0700

    set hot reload module

--- live-api/src/main.ts
+++ live-api/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Api Swagger')
+    .setDescription('The Live Api')
+    .setVersion('1.0')
+    .addTag('CarLive')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
+
+  if(module.hot){
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
 }
 bootstrap();
